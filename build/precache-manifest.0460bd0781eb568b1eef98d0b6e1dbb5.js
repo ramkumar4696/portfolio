5d7cb4cf517e5b99567713869247ccede7814025
ram <ram.j4696@gmail.com>
2020-09-13 21:24:32 +0530

    update social links

---
+++ build/precache-manifest.0460bd0781eb568b1eef98d0b6e1dbb5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2738984484a80456908c56c926da8041",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb95d98ee552dee5be7",
+    "url": "/static/css/2.902dce5a.chunk.css"
+  },
+  {
+    "revision": "1e21a4279e9c028f503e",
+    "url": "/static/css/main.647dc7a5.chunk.css"
+  },
+  {
+    "revision": "afb95d98ee552dee5be7",
+    "url": "/static/js/2.99e08562.chunk.js"
+  },
+  {
+    "revision": "1e21a4279e9c028f503e",
+    "url": "/static/js/main.cdf4e4a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "745b7d55639dbed2e09043e492230e5c",
+    "url": "/static/media/Spinner.745b7d55.gif"
+  },
+  {
+    "revision": "43f62b103c33f53ea8becff3f1725ac6",
+    "url": "/static/media/Vmarine.43f62b10.jpg"
+  },
+  {
+    "revision": "851ac5cd41c1272715245e19eb765cce",
+    "url": "/static/media/aguaDeLuz.851ac5cd.png"
+  },
+  {
+    "revision": "0a799148a50bb02c6f380eabd8d97559",
+    "url": "/static/media/fontawesome-webfont.0a799148.svg"
+  },
+  {
+    "revision": "3293616ec0c605c7c2db25829a0a509e",
+    "url": "/static/media/fontawesome-webfont.3293616e.woff"
+  },
+  {
+    "revision": "8b27bc96115c2d24350f0d09e6a9433f",
+    "url": "/static/media/fontawesome-webfont.8b27bc96.eot"
+  },
+  {
+    "revision": "dcb26c7239d850266941e80370e207c1",
+    "url": "/static/media/fontawesome-webfont.dcb26c72.ttf"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/static/media/todolist.ac82d97d.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/git-portfolio/precache-manifest.d509f2eb160dd0dd8a2027de132a3ae6.js"
+  "/precache-manifest.0460bd0781eb568b1eef98d0b6e1dbb5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/git-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
